531a4c87bbc2c56ae2b3ea237709d5882e184d9b
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-06-23 18:34:38 +0500

    default_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:default_project/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'local/storage_repository.dart';
 
-
-void main(){
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageRepository.getInstance();
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: HomeScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        ),
+        home: const HomeScreen(),
+      );},
     );
   }
 }
-
