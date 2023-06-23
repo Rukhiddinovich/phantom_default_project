e1e4b15b2dfbe6a5f2fa3dbae03576be25eb50b6
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-06-23 17:44:09 +0500

    amaliy_22iyun

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,38 @@
-import 'package:default_project/ui/home/home_screen.dart';
+import 'package:default_project/local/storage_repository.dart';
+import 'package:default_project/ui/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
-void main(){
+
+Future<void>main()async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageRepository.getInstance();
+  
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: HomeScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+            useMaterial3: true,
+          ),
+          initialRoute: RouteNames.splash,
+          onGenerateRoute: AppRoutes.generateRoute,
+        );
+      },
     );
   }
 }
-
