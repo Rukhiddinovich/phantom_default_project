c6dd3fcd9d8ca1cb4d63745f4a7b647986b9c431
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-07-07 09:26:03 +0500

    default_project

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,7 +1,4 @@
-import 'package:default_project/local/storage_repository.dart';
-import 'package:default_project/ui/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,169 +8,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-
-  Future<void> _init() async {
-    _lastNameController.text = StorageRepository.getString("lastName");
-    _nameController.text = StorageRepository.getString("name");
-  }
-
-  @override
-  void initState() {
-    _init();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white.withOpacity(0),
-        toolbarHeight: 0.h,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20.h,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.r), color: Colors.white),
-                child: TextField(
-                  controller: _nameController,
-                  cursorHeight: 22.h,
-                  cursorWidth: 1.w,
-                  style: const TextStyle(color: Colors.black),
-                  cursorColor: Colors.black,
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 12.w, vertical: 15.h),
-                    border: InputBorder.none,
-                    labelText: "Name",
-                    labelStyle: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15.sp,
-                        color: Colors.grey,
-                        fontFamily: "Poppins"),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.r), color: Colors.white),
-                child: TextField(
-                  controller: _lastNameController,
-                  cursorHeight: 22.h,
-                  cursorWidth: 1.w,
-                  style: const TextStyle(color: Colors.black),
-                  cursorColor: Colors.black,
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 12.w, vertical: 15.h),
-                    border: InputBorder.none,
-                    labelText: "Last Name",
-                    labelStyle: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15.sp,
-                        color: Colors.grey,
-                        fontFamily: "Poppins"),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.r),
-                      borderSide: BorderSide(width: 1.w, color: Colors.grey),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            ElevatedButton(
-              child: Text(
-                "Go Second Page",
-                style: TextStyle(
-                  fontFamily: "Poppins",
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              onPressed: () {
-                setState(() {
-                  _saveString("name", _nameController.text);
-                  _saveString("lastName", _lastNameController.text);
-                });
-                if (_nameController.text.isEmpty ||
-                    _lastNameController.text.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        shape: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.teal),
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(20.r),topRight: Radius.circular(20.r)),
-                        ),
-                    backgroundColor: Colors.teal,
-                    content: Text("Fill is Empty",
-                        style: TextStyle(
-                            fontSize: 20.sp,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black)),
-                  ));
-                }else{
-                  Navigator.pushNamed(context, RouteNames.secondScreen,arguments: <String, String>{
-                    "name_text":_nameController.text,
-                    "lastName_text":_lastNameController.text,
-                  });
-                }
-              },
-            )
-          ],
-        ),
+        backgroundColor: Colors.teal,
+        title: const Text("Default Project"),
+        centerTitle: true,
       ),
     );
-  }
-
-  _saveString(String key, String value) async {
-    await StorageRepository.putString(key, value);
   }
 }
