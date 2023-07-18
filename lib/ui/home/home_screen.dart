87cf627cc6faa56cc36873694072a9728d740260
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-07-18 18:14:05 +0500

    serper_app

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -174,7 +174,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-            SizedBox(width: 11.w),
+            SizedBox(width: 10.w),
             Expanded(
               child: ListView.builder(
                 controller: scrollController,
