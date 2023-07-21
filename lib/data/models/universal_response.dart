a16ebec5ba91e09d78ea449978f3c8a7d375a131
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-07-21 13:38:52 +0500

    default_project theme and text_theme and local_database

--- lib/data/models/universal_response.dart
+++ lib/data/models/universal_response.dart
@@ -2,8 +2,10 @@ class UniversalResponse {
   UniversalResponse({
     this.error = "",
     this.data,
+    this.statusCode=0,
   });
 
   dynamic data;
   String error;
+  int statusCode;
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,6 +4,9 @@ class AppColors{
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color C_00B2FF= Color(0xFF00B2FF);
+  static const Color C_30A2C5=Color(0xFF30A2C5);
+  static  Color textColor = const Color(0xFFFFFFFF).withOpacity(0.87);
+  static const Color passiveTextColor =  Color(0xFFAFAFAF);
 
 
 
